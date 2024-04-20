c60e6bc0197efd47b8401636415547df606fcdee
Nazirov <nazirovelmurod6@gmail.com>
2024-04-20 21:32:21 +0500

    Nazirov 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ public class Program
         try
         {
             // Nazirov Elmurod
+            // Nazirov Elmurod 2
             // Sulaymanov Islom
+
+
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
