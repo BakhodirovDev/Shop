f796e1a24dec342f5a8f03c3b111996edbd732d0
Nazirov <nazirovelmurod6@gmail.com>
2024-04-20 21:29:26 +0500

    Nazirov's name added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ public class Program
     {
         try
         {
+            // Nazirov Elmurod
+
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
