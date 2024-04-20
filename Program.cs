392717e53ccd7f79a749257b6cea0260f5cf181b
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-04-20 22:41:07 +0500

    546

--- Program.cs
+++ Program.cs
@@ -37,8 +37,7 @@ public class Program
             // Behruz
 
             //HAMMA UYGA GOOO
-            // Mirjahon bulli ketdik
-
+            // ok
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
