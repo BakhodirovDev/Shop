61547542fa2713e3e1033117ab65562519de4d65
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-04-20 21:52:37 +0500

    add

--- Program.cs
+++ Program.cs
@@ -7,12 +7,7 @@ public class Program
     {
         try
         {
-<<<<<<< HEAD
             // Behruz
-=======
-            //Javohir Aliyev
-
->>>>>>> 007e4a1415b845e030613f6f0e0c9f3050f0fa8d
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
