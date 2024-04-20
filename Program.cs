a79a0284a0812796b0ccba33752db1d12b926279
Sarvar <jabborovsarvar06@gmail.com>
2024-04-20 21:32:58 +0500

    sarvar

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Program
     {
         try
         {
-            // sarvar 
+            // sarvar
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
