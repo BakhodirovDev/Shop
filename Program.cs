0da7e6d3c2a6b7004ae928630b97adfbbe8dc477
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-04-23 17:23:33 +0500

    vcbnfgh

--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
 ﻿using Shop;
-using Shop.Function;
 
 public class Program
 {
     public static void Main()
     {
         try
-        {   
-<<<<<<< HEAD
+        {
             // Behruz
-=======
-            //QuvonchBek
->>>>>>> da7a3c5d2b1553a9b8bcd9d244050fd17c379504
+
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
