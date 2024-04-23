01a2b8da3cb5ac25540cbe556f3af314915f89a2
Quvonchbek <MuqimovQuvonch003@gamil.com>
2024-04-23 17:13:59 +0500

    ertsert

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Program
     {
         try
         {   
-            // ghmhjmghjm
+            //QuvonchBek
             Console.Title = "Online Shopping System";
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Main_Shop.main();
